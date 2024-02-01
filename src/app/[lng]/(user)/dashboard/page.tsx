133f3fddee831ee4f663bc0894934bc31ec1b134
Miroslav Petrik <miroslavpetrik@outlook.com>
2024-02-01 11:28:58 +0100

    i18n: dashboard

--- src/app/[lng]/(user)/dashboard/page.tsx
+++ src/app/[lng]/(user)/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { selectCurrentUser } from "@/edgedb/user";
+import { useTranslation } from "@/i18n";
 import { type Params } from "@/types";
 import { redirect } from "next/navigation";
 
@@ -9,10 +10,12 @@ export default async function Dashboard({ params: { lng } }: Params) {
     redirect("/onboarding");
   }
 
+  const { t } = await useTranslation("dashboard", lng);
+
   return (
     <div className="pt-16">
       <h1 className="text-4xl font-extrabold tracking-tight">
-        Welcome {user.name}
+        {t("welcome", { name: user.name })}
       </h1>
     </div>
   );
