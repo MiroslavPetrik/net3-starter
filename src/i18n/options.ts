4a3375bdcd1f2715406c26828807221ea20cba74
Miroslav Petrik <miroslavpetrik@outlook.com>
2024-03-15 22:43:40 +0100

    feat(i18n): add second lang

--- src/i18n/options.ts
+++ src/i18n/options.ts
@@ -2,15 +2,17 @@ import { type InitOptions } from "i18next";
 import { type Languages } from "./types";
 
 export const fallbackLng = "en";
-export const languages = [fallbackLng] as const; // add your locales (e.g. "de", "sk") into the list
+export const languages = [fallbackLng, "sk"] as const; // add your locales (e.g. "de", "sk") into the list
 export const i18nCookieName = "i18next" as const;
 
 // For each lang in languages, import your zod mapping
 import enZod from "zod-i18n-map/locales/en/zod.json";
+import skZod from "zod-i18n-map/locales/sk/zod.json";
 import { zodErrorNameSpace } from "./zodError";
 
 const resources: Record<Languages, { zod: Record<string, unknown> }> = {
   en: { zod: enZod },
+  sk: { zod: skZod },
 };
 
 export function getOptions(ns: string, lng = fallbackLng) {
