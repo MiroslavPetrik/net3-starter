af3ee832c577f3b9b9d49cc7cf216dce64245dba
Miroslav Petrik <miroslavpetrik@outlook.com>
2024-03-15 18:15:41 +0100

    fix(flowbite): import for tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,7 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 import { variable } from "./src/styles/font/variable";
 
 export default {
-  content: [
-    "./src/**/*.tsx",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.tsx", "node_modules/flowbite-react/lib/esm/**/*.js"],
   theme: {
     extend: {
       fontFamily: {
