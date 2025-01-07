704831280e2dd590978fd1636bedbefff35aef8b
Miroslav Petrik <miroslavpetrik@outlook.com>
2025-01-07 19:27:53 +0100

    docs: i18n parser for translate

--- i18next-parser.config.ts
+++ i18next-parser.config.ts
@@ -13,6 +13,11 @@ export default {
         lexer: "JsxLexer",
         // See file /src/i18n/castString.ts
         transIdentityFunctionsToIgnore: ["castString"],
+        /**
+         * We use custom translate (import { translate } from "@/i18n") function.
+         * The "useTranslation" (as async function) cannot be used in server components,
+         * because Next.js will complain that hooks are allowed only in client components.
+         */
         namespaceFunctions: ["useTranslation", "translate"],
       },
     ],
