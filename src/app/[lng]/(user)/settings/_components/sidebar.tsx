e1a91478a3e734d801139522e561dea7cef0a8e4
Miroslav Petrik <miroslavpetrik@outlook.com>
2025-01-15 11:47:17 +0100

    fix(settings-sidebar): use next/link to navigate to preserve react state

--- src/app/[lng]/(user)/settings/_components/sidebar.tsx
+++ src/app/[lng]/(user)/settings/_components/sidebar.tsx
@@ -1,13 +1,16 @@
 "use client";
-import type { LanguageParam } from "@/i18n";
-import { useLngPathname } from "@/i18n/client";
+
+import Link from "next/link";
+
 import {
   Sidebar,
   SidebarItem,
   SidebarItems,
   SidebarItemGroup,
-} from "flowbite-react";
+} from "flowbite-react/components/Sidebar";
 import { useTranslation } from "react-i18next";
+import type { LanguageParam } from "@/i18n";
+import { useLngPathname } from "@/i18n/client";
 
 export const SettingsSidebar = ({ lng }: LanguageParam) => {
   const pathname = useLngPathname(lng);
@@ -18,12 +21,14 @@ export const SettingsSidebar = ({ lng }: LanguageParam) => {
       <SidebarItems>
         <SidebarItemGroup>
           <SidebarItem
+            as={Link}
             href="/settings/profile"
             active={pathname.startsWith("/settings/profile")}
           >
             {t("editProfile.title")}
           </SidebarItem>
           <SidebarItem
+            as={Link}
             href="/settings/email"
             active={pathname.startsWith("/settings/email")}
           >
@@ -32,6 +37,7 @@ export const SettingsSidebar = ({ lng }: LanguageParam) => {
         </SidebarItemGroup>
         <SidebarItemGroup>
           <SidebarItem
+            as={Link}
             href="/settings/delete-account"
             active={pathname.startsWith("/settings/delete-account")}
           >
